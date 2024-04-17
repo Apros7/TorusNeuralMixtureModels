f60edd26ee830bee8a2ce2d5b10646c0b95b97e4
Your Name <you@example.com>
2024-04-17 12:55:48 +0200

    Revert "Adding NCE method"
    This reverts commit bb10d63e2898461ad312e2af36ffa683a288bfd2.

---
+++ NCE.py
@@ -0,0 +1,27 @@
+## torch class
+
+import torch as torch
+import numpy 
+import math
+from torch import logsumexp
+from torch import logexp
+
+# en init funktion containing all parameters for NCE
+
+def NCE(torch.nn.Module):
+
+    def __init__(self, c, ptilde, phi):
+        super(NCE, self).__init__()
+        self.c = c
+        self.ptilde = ptilde
+        self.phi = phi
+
+    def log_p_TG(self, ptilde, c):
+        return torch.logexp(ptilde + c)
+    
+    def log_p_TGMM(self, ptilde, c):
+        return torch.logsumexp(ptilde + c) 
+
+    def loss(self, phi, c, noise):
+        raise('implement loss function here')
+        
