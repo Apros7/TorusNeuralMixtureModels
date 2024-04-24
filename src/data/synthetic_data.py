6cf8c678e5dcbf400377e7149dde91dfda9dcc75
apros7 <lucas.vilsen@gmail.com>
2024-04-24 14:03:37 +0200

    src data

---
+++ src/data/synthetic_data.py
@@ -0,0 +1,17 @@
+## Script for creating synthetic data as described in [Klein2020]
+
+## We need to make a script for creating synthetic data outside of Torus Graphs
+## And another script for sampling in Torus Graphs.
+
+from typing import List, Tuple
+
+def sampleFromTorusGraph(nodes : int, samples : int, nodepairs : List[Tuple[int, int]]):
+    # 
+    pass
+
+def sampleOutsideOfTorusGraph():
+    drawVonMises()
+    pass
+
+def drawVonMises():
+    pass
