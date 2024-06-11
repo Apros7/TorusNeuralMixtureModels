4970eed883bb8d6fc2284de988f436a4af688c6d
apros7 <lucas.vilsen@gmail.com>
2024-06-11 12:34:34 +0200

    torus graph

--- src/models/torusGraph.py
+++ src/models/torusGraph.py
@@ -1,9 +1,15 @@
 from src.data.synthetic_data import sampleFromTorusGraph
 
 class TorusGraph():
-    def __init__(self, data, estimation_func : Function = NCE):
+    def __init__(self, data, estimation_func): # estimation_func can be NCE or SM
         self.data = data
         self.estimate_params(estimation_func)
+        self.nodes = 0
+        self.nodePairs = 0
+        self.estimate_phi()
+        
+    def estimate_phi(self):
+        self.phi, self.c = "hey", "hey"
 
-    def sample(samples : int = 100):
+    def sample(self, samples : int = 100):
         return sampleFromTorusGraph(self.nodes, samples, phi = self.phi, nodePairs=self.nodePairs)
