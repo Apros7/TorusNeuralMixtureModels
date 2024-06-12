58cafd62e3771c368608c5ce6909f80bbb0f07b9
Your Name <you@example.com>
2024-06-12 12:31:19 +0200

    changing branch

--- src/plots/nce_vs_sm.py
+++ src/plots/nce_vs_sm.py
@@ -9,10 +9,10 @@ sys.path.insert(0, '.')
 from src.parameterEstimation.trainNCE import mixture_torch_loop
 from src.parameterEstimation.NCE import TorusGraphs
 from src.data.synthetic_data import sampleFromTorusGraph
+from src.parameterEstimation.scoreMatching import SM
 
 
-def plot_nce(N:int, nodes:int, K:int, phi:np.ndarray = None):
-   X = sampleFromTorusGraph(nodes, N, phi = phi, fitFCM=False, fitPAD=True, fitPAS=False)
+def plot_nce(X, nodes, K):
    X = torch.from_numpy(X).float().T
    noise = torch.rand(N,nodes)*2*torch.tensor(np.pi) 
 
@@ -32,7 +32,7 @@ def plot_nce(N:int, nodes:int, K:int, phi:np.ndarray = None):
    plt.subplot(1,2,2)
    plt.imshow(theta.detach().numpy()[0,:,:])
    plt.colorbar()
-   plt.title("Theta")
+   plt.title("Theta, NCE")
    plt.xlabel("Dimension")
    plt.ylabel("Model")
 
@@ -40,30 +40,36 @@ def plot_nce(N:int, nodes:int, K:int, phi:np.ndarray = None):
    plt.show()
 
 
-def plot_sm(nodes:int, N:int, phi:np.ndarray = None):
-    phi = score_matching(phi, nodes, N)
-    plt.imshow(phi)
-    plt.colorbar()
-    plt.title("Theta")
-    plt.xlabel("Dimension")
-    plt.ylabel("Model")
+def plot_sm(X, datamodel):
+   phi, covPhi = SM(X, datamodel).compPhiHatAndCovPhiHat()
+   plt.imshow(phi)
+   plt.colorbar()
+   plt.title("Theta, score matching")
+   plt.xlabel("Dimension")
+   plt.ylabel("Model")
 
-    plt.savefig('plot_nce.png')
-    plt.show()
+   plt.savefig('plot_sm.png')
+   plt.show()
 
-
-
-    return 0
   
 
 
 
 
 if __name__ == "__main__":
-
    N = 100 # samples
    nodes = 3
    K = 1 # number of models (or components)
    phi = np.block([ 0, 0, 8*np.cos(np.pi), 8*np.sin(np.pi), 0, 0 ])
-   plot_nce(N, nodes, K, phi)
-   plot_sm(nodes, N, phi)
+   X, datamodel = sampleFromTorusGraph(
+         nodes = nodes,
+         samples = N,
+         phi = phi,
+         fitFCM = False,
+         fitPAD = True,
+         fitPAS = False,
+         return_datamodel = True
+      )
+
+   plot_nce(X, nodes, K)
+   plot_sm(X, datamodel)
