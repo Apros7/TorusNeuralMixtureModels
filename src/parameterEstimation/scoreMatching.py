0efd607d90a008c9071cb879189b9d81fb6806d4
Lucas Vilsen <97256863+Apros7@users.noreply.github.com>
2024-06-12 12:22:55 +0200

    init (#13)

--- src/parameterEstimation/scoreMatching.py
+++ src/parameterEstimation/scoreMatching.py
@@ -1,34 +1,49 @@
+import sys
+sys.path.insert(0, '.')
+
 import numpy as np
 import scipy.linalg
+from tqdm import tqdm
+
+from src.data.synthetic_data import TorusGraphInformation
 
 #X = np.array([ [-2.1,-1.4,2,2.8,-0.7,-2], [1.3,-2.9,1.2,-2.9,1.7,-0.1], [-2.9,-2.5,-1.1,-0.4,1.9,-0.3] ])
 #selMode = (True, True, True)
 #num = {'nodes': 3, 'trials': 6, 'nodePairs': 3, 'param': 18}
 #nodePairs = {'nodes': np.array([ [0,1], [0,2], [1,2] ])}
 
+class TorusGraphInformation:
+    samples: int # trials
+    nodes: int
+    nodePairs: np.ndarray[np.ndarray[int, int]]
+    nNodePairs: int
+    params: int
+    fitFCM: bool    # FirstCircularMoments
+    fitPAD: bool    # PairwiseAngleDifferences
+    fitPAS: bool    # PairwiseAngleSums
+
 class SM():
-    def __init__(self, X, t, num, selMode, nodePairs, 
-                    sC, sS, sAlpha, sBeta, sGamma, sDelta ):
+    def __init__(self, X, dm: TorusGraphInformation): # dm = TorusGraph datamodel with all information needed
         super(SM, self).__init__()
         self.X = X
-        self.t = t
-        self.num = num
-        self.selMode = selMode
-        self.nodePairs = nodePairs
-        self.sC = sC
-        self.sS = sS
-        self.sAlpha = sAlpha
-        self.sBeta = sBeta
-        self.sGamma = sGamma
-        self.sDelta = sDelta
+        self.selMode = (dm.fitFCM, dm.fitPAD, dm.fitPAS)
+        self.nodePairs = dm.nodePairs
+        self.nNodePairs = dm.nNodePairs
+        self.params = dm.params
+        self.trials = dm.samples
+        self.nodes = dm.nodes
         
 
     # Sufficient Statistics
-    def compSufStat(self, X, selMode, nodePairs):
+    def compSufStat(self):
+        X = self.X
+        nodePairs = self.nodePairs
+        selMode = self.selMode
+        self.nodes
 
         # Sufficient Statistics
-        Xi = X[nodePairs['nodes'][:,0],:]
-        Xj = X[nodePairs['nodes'][:,1],:]
+        Xi = X[nodePairs[:,0],:]
+        Xj = X[nodePairs[:,1],:]
         H = []
         if selMode[0]:
             sC = np.cos(X)
@@ -63,13 +78,12 @@ class SM():
 
         return (H, sC, sS, sAlpha, sBeta, sGamma, sDelta)
 
-
-        
-
-
     #Gamma (Jacobian of the sufficient statistics )
-    def compGammaXt(self, t, num, selMode, nodePairs, 
-                    sC, sS, sAlpha, sBeta, sGamma, sDelta ):
+    def compGammaXt(self, t, sC, sS, sAlpha, sBeta, sGamma, sDelta ):
+        selMode = self.selMode
+        nodePairs = self.nodePairs
+        nodes = self.nodes
+        nNodePairs = self.nNodePairs
 
         # define gammaXt (t-th trial)
         Dt = [] # Jacobian of the suficient statistics
@@ -80,15 +94,14 @@ class SM():
             Dt.extend(dC)
             Dt.extend(dS)
         if selMode[1]:
-            dAlpha = np.zeros((num['nodePairs'],num['nodes']))
-            dBeta =  np.zeros((num['nodePairs'],num['nodes']))
+            dAlpha = np.zeros((nNodePairs,nodes))
+            dBeta =  np.zeros((nNodePairs,nodes))
         if selMode[2]:
-            dGamma = np.zeros((num['nodePairs'],num['nodes']))
-            dDelta = np.zeros((num['nodePairs'],num['nodes']))
+            dGamma = np.zeros((nNodePairs,nodes))
+            dDelta = np.zeros((nNodePairs,nodes))
 
-        for r in range(num['nodePairs']):
-            colInds = [ nodePairs['nodes'][r,0], \
-                        nodePairs['nodes'][r,1] ]
+        for r in range(nNodePairs):
+            colInds = [ nodePairs[r,0], nodePairs[r,1] ]
             if selMode[1]:
                 dAlpha[r, colInds] = np.array([-1,  1]) * sBeta[r, t]
                 dBeta[r, colInds]  = np.array([ 1, -1]) * sAlpha[r, t]
@@ -108,30 +121,30 @@ class SM():
         return gammaXt
 
     #Phi hat and Covariance of Phi hat
-    def compPhiHatAndCovPhiHat(self, X, num, selMode, nodePairs):
+    def compPhiHatAndCovPhiHat(self):
+        param = self.params
+        trials = self.trials
 
         # Sufficient Statistics
-        H, sC, sS, sAlpha, sBeta, sGamma, sDelta = \
-            self.compSufStat(X, selMode, nodePairs)
+        H, sC, sS, sAlpha, sBeta, sGamma, sDelta = self.compSufStat()
         Ht = H.T
 
         # define compPhiHatAndCovPhiHat
         HhatX = np.mean(H,1) # H is (n_params, n_trials)
 
         # gammaHatX: average of gammaXt over trials
-        gammaHatX = np.zeros((num['param'],num['param']))
+        gammaHatX = np.zeros((param, param))
         cache_gammaXt = True
         try:
-            gammaXt_trials = np.zeros((num['trials'],num['param'],num['param']))
+            gammaXt_trials = np.zeros((trials,param,param))
         except MemoryError:
             cache_gammaXt = False
-        for t in range(num['trials']):
-            gammaXt = self.compGammaXt(t, num, selMode, nodePairs,
-                                sC, sS, sAlpha, sBeta, sGamma, sDelta)
+        for t in tqdm(range(trials), "Running Score Matching"):
+            gammaXt = self.compGammaXt(t, sC, sS, sAlpha, sBeta, sGamma, sDelta)
             if cache_gammaXt:
                 gammaXt_trials[t, :, :] = gammaXt
             gammaHatX += gammaXt
-        gammaHatX /= num['trials']   
+        gammaHatX /= trials
 
         # Estimating phiHat
         L = scipy.linalg.cholesky(gammaHatX + 1e-8 * np.eye(gammaHatX.shape[0]), lower=True)
@@ -139,21 +152,19 @@ class SM():
 
 
         # Estimating the covariance of phiHat: covPhiHat
-        vHatX = np.zeros((num['param'],num['param']))
-        for t in range(num['trials']):
+        vHatX = np.zeros((param,param))
+        for t in range(trials):
             if cache_gammaXt:
 
                 vVec = np.dot(gammaXt_trials[t, :, :], phiHat) - Ht[t, :]
             else:
-                gammaXt = self.compGammaXt(t, num, selMode, nodePairs,
-                                sC, sS, sAlpha, sBeta, sGamma, sDelta)
-
+                gammaXt = self.compGammaXt(t,sC, sS, sAlpha, sBeta, sGamma, sDelta)
                 vVec = np.dot(gammaXt, phiHat) - Ht[t, :]
 
             vHatX += np.outer(vVec, vVec)
-        vHatX /= num['trials']  
+        vHatX /= trials 
 
         Linv = np.linalg.inv(L)
-        covPhiHat = 1/num['trials'] * scipy.linalg.cho_solve((L, True), vHatX) @ Linv.T @ Linv
+        covPhiHat = 1/trials * scipy.linalg.cho_solve((L, True), vHatX) @ Linv.T @ Linv
 
         return (phiHat, covPhiHat)
