ad47f04359dc5851e6788a699376503ef0d782d1
Your Name <you@example.com>
2024-06-14 11:56:03 +0200

    fix for train nce

--- src/parameterEstimation/trainNCE.py
+++ src/parameterEstimation/trainNCE.py
@@ -16,10 +16,12 @@ def mixture_torch_loop(X,noise,model):
     objective = []
 
 
-    for epoch in tqdm(range(10000), desc="NCE training"):
-            
-        obj, log_prop_data = model.NCE_objective_function(X,noise)
-        obj = -obj
+    for epoch in tqdm(range(2000), desc="NCE training"):
+        if model.return_log_prop_data:    
+            obj, log_prop_data = model.NCE_objective_function(X,noise)
+            obj = -obj
+        else:
+            obj = -model.NCE_objective_function(X,noise)
 
 
         if torch.isnan(-obj):
