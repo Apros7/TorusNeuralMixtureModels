220881378a941484b31cfc1adddc5fd3949086d3
Your Name <you@example.com>
2024-06-14 13:48:57 +0200

    file to clean and import data

--- src/data/sleep_data_clean.py
+++ src/data/sleep_data_clean.py
@@ -11,6 +11,4 @@ sys.path.insert(0,'.')
 
 # første element i phases indeholderne målingerne for første element i stages.
 
-sub1 = pd.read_csv('src/data/phases_7/sub-01_mainlysleep_phases.txt', header=None)
-sub1.columns = ['mainlysleep_phases']
-sub2 = pd.read_csv('src/data/phases_7/sub-04_mainlysleep_phases.txt', header=None)
+
