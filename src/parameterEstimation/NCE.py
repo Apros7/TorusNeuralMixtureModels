06f1157e7c96ac53e2cd0ed9d7c41ddba3f342da
Your Name <you@example.com>
2024-06-14 10:44:01 +0200

    updated nce

--- src/parameterEstimation/NCE.py
+++ src/parameterEstimation/NCE.py
@@ -4,93 +4,95 @@ import torch.nn as nn
 
 
 class TorusGraphs(nn.Module):
-   def __init__(self, nodes:int, K:int):
-       '''
-       nodes: number of nodes
-       K: number of models (or components) 
-       
-       '''
-       super().__init__()
+    def __init__(self, nodes:int, K:int, return_log_prop_data: bool = False):
+        '''
+        nodes: number of nodes
+        K: number of models (or components) 
+        
+        '''
+        super().__init__()
 
 
-       self.K = K
-       self.nodes = nodes
-       z = nodes*(nodes-1)//2
-       self.K = 1
-       self.theta = nn.Parameter(torch.randn(self.K,2,z))
-       self.logc = nn.Parameter(torch.zeros(self.K))
+        self.K = K
+        self.nodes = nodes
+        z = nodes*(nodes-1)//2
+        self.K = 1
+        self.theta = nn.Parameter(torch.randn(self.K,2,z))
+        self.logc = nn.Parameter(torch.zeros(self.K))
+        self.return_log_prop_data = return_log_prop_data
 
 
-       self.triu_indices = torch.triu_indices(nodes,nodes,offset=1)
+        self.triu_indices = torch.triu_indices(nodes,nodes,offset=1)
 
 
-   def NCE_objective_function(self,X,noise):
-       
-       """This function computes the NCE objective function for an arbitrary number of the Torus Graphs models.
-       Thus it works for mixture models as well.
-       Args:
-         X: torch.tensor of shape (N,nodes) where N is the number of samples and nodes is the number of nodes
-    noise: torch.tensor of shape (N,nodes) where N is the number of samples and nodes is the number of nodes
+    def NCE_objective_function(self,X,noise):
+        
+        """This function computes the NCE objective function for an arbitrary number of the Torus Graphs models.
+        Thus it works for mixture models as well.
+        Args:
+            X: torch.tensor of shape (N,nodes) where N is the number of samples and nodes is the number of nodes
+        noise: torch.tensor of shape (N,nodes) where N is the number of samples and nodes is the number of nodes
 
-         Returns:
-            J: torch.tensor of shape (K) where K is the number of models
-            If you do:  
+            Returns:
+                J: torch.tensor of shape (K) where K is the number of models
+                If you do:  
 
-            model = TorusGraphs(nodes=X.shape[1],K=1)
-            model,objective = mixture_torch_loop(X,noise,model)
+                model = TorusGraphs(nodes=X.shape[1],K=1)
+                model,objective = mixture_torch_loop(X,noise,model)
 
-            Then you can get theta and c by doing:
-            theta = model.theta
-            logc = model.logc
+                Then you can get theta and c by doing:
+                theta = model.theta
+                logc = model.logc
 
-    
-       
-    """
+        
+        
+        """
 
-       # theta (Mx2xz)
-       # x (Nxp)
-       N = X.shape[0]
-       M = noise.shape[0] #number of noise samples
-
-
-       log_prob_data = torch.zeros(self.K,N)
-       log_prob_noise = torch.zeros(self.K,N)
-    #    for k in range(self.K):
-    #        for i in range(N):
-    #            for z in range(self.p*(self.p-1)//2):
-    #                cosx = torch.cos(X[i,self.triu_indices[0,z]] - X[i,self.triu_indices[1,z]])
-    #                sinx = torch.sin(X[i,self.triu_indices[0,z]] - X[i,self.triu_indices[1,z]])
-    #                log_prob_data[k,i] += self.theta[k,:,z]@torch.tensor([cosx,sinx])
+        # theta (Mx2xz)
+        # x (Nxp)
+        N = X.shape[0]
+        M = noise.shape[0] #number of noise samples
 
 
-    #                cosn = torch.cos(noise[i,self.triu_indices[0,z]] - noise[i,self.triu_indices[1,z]])
-    #                sinn = torch.sin(noise[i,self.triu_indices[0,z]] - noise[i,self.triu_indices[1,z]])
-    #                log_prob_noise[k,i] += self.theta[k,:,z]@torch.tensor([cosn,sinn])
-       
-
-       for z in range(self.nodes*(self.nodes-1)//2):
-           cosx = torch.cos(X[:,self.triu_indices[0,z]] - X[:,self.triu_indices[1,z]])
-           sinx = torch.sin(X[:,self.triu_indices[0,z]] - X[:,self.triu_indices[1,z]])
-           log_prob_data = self.theta[:,:,z]@torch.stack([cosx,sinx],dim=0)
-
-
-           cosn = torch.cos(noise[:,self.triu_indices[0,z]] - noise[:,self.triu_indices[1,z]])
-           sinn = torch.sin(noise[:,self.triu_indices[0,z]] - noise[:,self.triu_indices[1,z]])
-           log_prob_noise = self.theta[:,:,z]@torch.stack([cosn,sinn],dim=0)
-       log_prob_data = torch.logsumexp(log_prob_data + self.logc,dim=0)
-       log_prob_noise = torch.logsumexp(log_prob_noise + self.logc,dim=0)
+        log_prob_data = torch.zeros(self.K,N)
+        log_prob_noise = torch.zeros(self.K,N)
+        #    for k in range(self.K):
+        #        for i in range(N):
+        #            for z in range(self.p*(self.p-1)//2):
+        #                cosx = torch.cos(X[i,self.triu_indices[0,z]] - X[i,self.triu_indices[1,z]])
+        #                sinx = torch.sin(X[i,self.triu_indices[0,z]] - X[i,self.triu_indices[1,z]])
+        #                log_prob_data[k,i] += self.theta[k,:,z]@torch.tensor([cosx,sinx])
 
 
-       log_nx = torch.zeros(N) #needs to be implemented, this is the noise samples
-       log_ny = torch.zeros(N) # maybe just noise
+        #                cosn = torch.cos(noise[i,self.triu_indices[0,z]] - noise[i,self.triu_indices[1,z]])
+        #                sinn = torch.sin(noise[i,self.triu_indices[0,z]] - noise[i,self.triu_indices[1,z]])
+        #                log_prob_noise[k,i] += self.theta[k,:,z]@torch.tensor([cosn,sinn])
+        
+
+        for z in range(self.nodes*(self.nodes-1)//2):
+            cosx = torch.cos(X[:,self.triu_indices[0,z]] - X[:,self.triu_indices[1,z]])
+            sinx = torch.sin(X[:,self.triu_indices[0,z]] - X[:,self.triu_indices[1,z]])
+            log_prob_data = self.theta[:,:,z]@torch.stack([cosx,sinx],dim=0)
 
 
-       log_J1_denom = torch.logsumexp(torch.stack([torch.log(torch.tensor(N))+log_prob_data,torch.log(torch.tensor(M))+log_nx],dim=-1), dim=-1)
-       log_J2_denom = torch.logsumexp(torch.stack([torch.log(torch.tensor(N))+log_prob_noise,torch.log(torch.tensor(M))+log_ny],dim=-1), dim=-1)
+            cosn = torch.cos(noise[:,self.triu_indices[0,z]] - noise[:,self.triu_indices[1,z]])
+            sinn = torch.sin(noise[:,self.triu_indices[0,z]] - noise[:,self.triu_indices[1,z]])
+            log_prob_noise = self.theta[:,:,z]@torch.stack([cosn,sinn],dim=0)
+        log_prob_data = torch.logsumexp(log_prob_data + self.logc,dim=0)
+        log_prob_noise = torch.logsumexp(log_prob_noise + self.logc,dim=0)
 
 
-       J = torch.mean(torch.log(torch.tensor(N))+log_prob_data - log_J1_denom,dim=-1)+torch.mean(torch.log(torch.tensor(N))+log_ny - log_J2_denom,dim=-1)
+        log_nx = torch.zeros(N) #needs to be implemented, this is the noise samples
+        log_ny = torch.zeros(N) # maybe just noise
 
-    
 
-       return J
+        log_J1_denom = torch.logsumexp(torch.stack([torch.log(torch.tensor(N))+log_prob_data,torch.log(torch.tensor(M))+log_nx],dim=-1), dim=-1)
+        log_J2_denom = torch.logsumexp(torch.stack([torch.log(torch.tensor(N))+log_prob_noise,torch.log(torch.tensor(M))+log_ny],dim=-1), dim=-1)
+
+
+        J = torch.mean(torch.log(torch.tensor(N))+log_prob_data - log_J1_denom,dim=-1)+torch.mean(torch.log(torch.tensor(N))+log_ny - log_J2_denom,dim=-1)
+
+        if self.return_log_prop_data:
+            return (J, log_prob_data)
+        else:
+            return J
--- src/results/nce_compare_models.py
+++ src/results/nce_compare_models.py
@@ -38,38 +38,13 @@ X2 = torch.from_numpy(X2).float().T
 X = torch.cat([X1,X2],dim=0)
 
 
-noise = torch.rand(N*2,nodes)*2*torch.tensor(np.pi) # Noise distribution, mellem 0 og 2*pi
+noise = torch.rand(N,nodes)*2*torch.tensor(np.pi) # Noise distribution, mellem 0 og 2*pi
 
 
-models = TorusGraphs(nodes=X.shape[1],K=2)
-models,objective = mixture_torch_loop(X,noise,models)
+model1 = TorusGraphs(nodes=X1.shape[1],K=1,return_log_prop_data=True)
+model1,objective1, log_prob_data1 = mixture_torch_loop(X1,noise,model1)
+theta1,c1 = model1.theta,model1.logc
 
-
-theta = models.theta
-logc = models.logc
-
-# z : prob of point x belonging to model k
-# z = torch.exp(log_prop_data_1 + logc)/torch.sum(torch.exp(log_prop_data + logc),dim=0)
-def compute_log_probs(self, X):
-    N = X.shape[0]
-    log_prob_data = torch.zeros(self.K, N)
-    for k in range(self.K):
-        for i in range(N):
-            for z in range(self.nodes*(self.nodes-1)//2):
-                cosx = torch.cos(X[i, self.triu_indices[0, z]] - X[i, self.triu_indices[1, z]])
-                sinx = torch.sin(X[i, self.triu_indices[0, z]] - X[i, self.triu_indices[1, z]])
-                log_prob_data[k, i] += torch.sum(self.theta[k, 0, z]*cosx + self.theta[k, 1, z]*sinx)
-
-    z = torch.exp(log_prob_data + self.logc) / torch.sum(torch.exp(log_prob_data + self.logc), dim=0)
-    return z
-
-
-def classify_points(X):
-    log_probs = compute_log_probs(models, X)
-    class_labels = torch.argmax(log_probs, dim=1)
-    return class_labels
-
-labels = classify_points(X)
-# plot label dsitrubtion of the data
-plt.hist(labels.numpy())
-plt.show()
+model2 = TorusGraphs(nodes=X2.shape[1],K=1,return_log_prop_data=True)
+model2,objective2, log_prob_data2 = mixture_torch_loop(X2,noise,model2)
+theta2,c2 = model2.theta,model2.logc
